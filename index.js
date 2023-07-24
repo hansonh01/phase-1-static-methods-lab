385bdfd6ef7bc22d180b7ee13655e78c188e57d6
Hanson <hanson.hua903@gmail.com>
2023-07-23 19:09:19 -0500

    Complete

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 class Formatter {
   //add static methods here
-}
+  static capitalize(string){
+    if (typeof string !== 'string' || string.length === 0) {
+      return '';
+    }
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-' ]/g, '');
+  };
+
+  static titleize(string){
+    if (typeof string !== 'string' || string.length === 0) {
+      return '';
+    };
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ');
+
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      };
+    });
+    return this.capitalize(titleizedWords.join(' '));
+  };
+  
+};
